74839c00ebb1dcf3cec272e97707649ee2e4b13f
raidenlee3 <122454752+raidenlee3@users.noreply.github.com>
2023-01-11 08:43:49 -0500

    Create period_1_Lee_Raiden_StringFormatterA.java

---
+++ period_1_Lee_Raiden_StringFormatterA.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class StringFormatter
+{
+    /** Returns the total number of letters in wordList.
+     *  Precondition: wordList contains at least two words, consisting of letters only.
+     */
+public static int totalLetters(List<String> wordList)
+{
+    int total = 0;
+    for(String word : wordList)
+    {
+        total += word.length();
+    }
+    return total;
+}
+
+
+    public static void main(String[] args)
+    {
+         List<String> myWords = new ArrayList<String>();
+         myWords.add("A");
+         myWords.add("frog");
+         myWords.add("is");
+         System.out.println("Should print 7 and prints: " + totalLetters(myWords));
+
+         List<String>words2 = new ArrayList<String>();
+         words2.add("Hi");
+         words2.add("Bye");
+         System.out.println("Should print 5 and prints: " + totalLetters(words2));
+    }
+}
